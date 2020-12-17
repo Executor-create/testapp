0f3be2b90289ba2bf65e222cc8e6beb7f5108cc1
Executor-create <63257746+Executor-create@users.noreply.github.com>
2020-12-17 22:57:31 +0200

    Fix VotingSystem

--- TestApp/VotingSystem.cs
+++ TestApp/VotingSystem.cs
@@ -13,32 +13,38 @@ namespace TestApp
         static void Main(string[] args)
         {
             List<User> users = new List<User>();
-            users.Add(new User("Tom McConor", "qwrqr", "werwer"));
             users.Add(new User("Jack Doglas", "Jacki21", "qwets2"));
             users.Add(new User("Bob Robinson", "Bobik", "bwergr2"));
-            users.Add(new User("Elvis Cooper", "Elden", "234gdqw"));
+            users.Add(new User("Elvis Cooper", "Elden", "2gdqw"));
             users.Add(new User("Mike Clark", "Hadler", "irhn2"));
             foreach (User u in users)
             {
                 Console.WriteLine(u.GetName());
                 u.Enter();
-
+                
             }
-        }
-        private static void addUser()
-        {
-            
+
+            Console.WriteLine("----------------");
+
+            List<Candidate> candidates = new List<Candidate>();
+            candidates.Add(new Candidate("1.Donald Johnson"));
+            candidates.Add(new Candidate("2.Jonny Davis"));
+            candidates.Add(new Candidate("3.Harry Wilson"));
+            foreach(Candidate c in candidates)
+            {
+                Console.WriteLine(c.GetName());
+                c.getVoices();
+            }
+          
+            Console.ReadKey();
         }
         private static void findUser(User login, User password)
         {
             users.Find(currentUser => (currentUser == login) && (currentUser == password));
         }
-        private List<Candidate> getResults()
+        private static List<Candidate> getResults()
         {
             List<Candidate> candidates = new List<Candidate>();
-            candidates.Add(new Candidate("Donald Johnson"));
-            candidates.Add(new Candidate("Jonny Davis"));
-            candidates.Add(new Candidate("Harry Wilson"));
             return candidates;
         }
     }
