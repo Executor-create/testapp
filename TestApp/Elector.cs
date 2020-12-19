8cc192466d6dc579453ea901534f9f6d5229638f
Executor-create <63257746+Executor-create@users.noreply.github.com>
2020-12-19 20:51:37 +0200

    Fix elector

--- TestApp/Elector.cs
+++ TestApp/Elector.cs
@@ -4,18 +4,20 @@ using System.Text;
 
 namespace TestApp
 {
-    class Elector : User
+    public class Elector : User
     {
         private bool voted;
-        public Elector(bool Voted, string name, string login, string password)
+
+        public Elector(string name, string login, string password)
             : base(name, login, password)
         {
-            voted = Voted;
         }
+
         public bool IsVoted()
         {
-            return voted = false;
+            return voted;
         }
+
         public void Vote()
         {
             voted = true;
