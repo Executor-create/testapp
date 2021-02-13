24bb500925629191d4974179e1d9c25d2a0a9b95
Executor-create <63257746+Executor-create@users.noreply.github.com>
2021-02-13 11:21:11 +0200

    Add Admin

---
+++ TestApp/Admin.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestApp
+{
+    class Admin : User
+    {
+        private string login;
+        private string password;
+        private bool admin;
+
+        public Admin(string name, string login, string password)
+            :base (name, login, password)
+        {
+            this.login = login;
+            this.password = password;
+            admin = false;
+        }
+
+        public bool AdminEnter(string login, string password)
+        {
+            if (this.login == login && this.password == password)
+                return true;
+
+            return false;
+        }
+
+        public bool IsAdmin()
+        {
+            return admin = true;
+        }
+    }
+}
